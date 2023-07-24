84a0aeeffe9b5c54b2a48ee3c8f47167ca43611b
Brian Cheng <bgcheng27@gmail.com>
2023-07-24 11:11:31 -0700

    Successfully implemented DELETE method

--- client/src/components/ListItem.jsx
+++ client/src/components/ListItem.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 
-export default function ListItem({ text }) {
+export default function ListItem({ text, onDelete }) {
+
+  const handleDelete = (event) => {
+    event.preventDefault()
+    onDelete()
+  }
+
   return (
     <li>
       <label>
         <input type="checkbox" />
         <span>{text}</span>
       </label>
-      <button>Delete</button>
+      <button onClick={handleDelete}>Delete</button>
     </li>
   );
 }
--- client/src/components/TodoList.jsx
+++ client/src/components/TodoList.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ListItem from "./ListItem";
 
-export default function TodoList({ todos }) {
+export default function TodoList({ todos, onDelete}) {
+
   return (
     <div>
       <ul>
         {todos.map((todo) => {
-          return <ListItem key={todo._id} text={todo.text} />
+          return <ListItem key={todo._id} text={todo.text} onDelete={() => { onDelete(todo._id) }}/>
         })}
       </ul>
     </div>
